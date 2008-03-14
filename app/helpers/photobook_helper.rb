a1dc0a22b957f9514e4e74d42861caecefd7564c
Timothy Kim <timothykim@timothylive.net>
2008-03-13 23:23:54 -0400

    Photo uploader before fork

---
+++ app/helpers/photobook_helper.rb
@@ -0,0 +1,2 @@
+module PhotobookHelper
+end
---
+++ db/migrate/004_create_albums_and_photos.rb
@@ -0,0 +1,38 @@
+class CreateAlbumsAndPhotos < ActiveRecord::Migration
+  def self.up
+    create_table :albums do |t|
+      t.column :name, :string, :limit => 80
+      t.column :user_id, :integer
+      t.column :key_photo_id, :integer
+      t.column :is_public, :boolean, :default => false
+      t.timestamps
+    end
+
+    create_table :photos do |t|
+      t.column :name, :string, :limit => 80
+      t.column :caption, :text
+      t.column :album_id, :integer
+      t.column :user_id, :integer
+      
+      t.column :content_type, :string
+      t.column :filename, :string     
+      t.column :size, :integer
+      
+      # used with thumbnails, always required
+      t.column :parent_id,  :integer 
+      t.column :thumbnail, :string
+      
+      # required for images only
+      t.column :width, :integer  
+      t.column :height, :integer
+      
+      t.timestamps
+    end
+
+  end
+
+  def self.down
+    drop_table :albums
+    drop_table :photos
+  end
+end
