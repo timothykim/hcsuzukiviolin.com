b7564cc5653ffacc7acc4e8697818c457004d8cb
Timothy Kim <timothykim@timothylive.net>
2008-04-03 22:16:54 -0400

    cleaned up lot of account view

--- public/javascripts/account/signup.js
+++ public/javascripts/account/signup.js
@@ -20,92 +20,95 @@ function check(text, type) {
 }
 
 function check_form() {
-	if ($$('p.ok').length == 6) {
-		$('form_submit').enable();
+	
+	var fn = $('user_firstname');
+	if(!fn.up().hasClassName('fieldWithErrors') && check(fn.value, "name")){
+		fn.up().removeClassName("nok");
+		fn.up().addClassName("ok");
 	} else {
-		$('form_submit').disable();
+		fn.up().removeClassName("fieldWithErrors");
+		fn.up().removeClassName("ok");
+		fn.up().addClassName("nok");
+	}
+
+	var ln = $('user_lastname');
+	if(!ln.up().hasClassName('fieldWithErrors') && check(ln.value, "name")){
+		ln.up().removeClassName("nok");
+		ln.up().addClassName("ok");
+	} else {
+		ln.up().removeClassName("fieldWithErrors");
+		ln.up().removeClassName("ok");
+		ln.up().addClassName("nok");
+	}
+
+	var em = $('user_email');
+	if(!em.up().hasClassName('fieldWithErrors') && check(em.value, "email")){
+		em.up().removeClassName("nok");
+		em.up().addClassName("ok");
+	} else {
+		em.up().removeClassName("fieldWithErrors");
+		em.up().removeClassName("ok");
+		em.up().addClassName("nok");
+	}
+
+	var emc = $('user_email_confirmation');
+	if(!emc.up().hasClassName('fieldWithErrors') && em.up().hasClassName("ok") && check(emc.value, "email") && emc.value == $('user_email').value) {
+		emc.up().removeClassName("nok");
+		emc.up().addClassName("ok");
+	} else {
+		fn.up().removeClassName("fieldWithErrors");
+		emc.up().removeClassName("ok");
+		emc.up().addClassName("nok");
+	}
+	
+	var ps = $('user_password');
+	if(!ps.up().hasClassName('fieldWithErrors') && check(ps.value, "password")){
+		ps.up().removeClassName("nok");
+		ps.up().addClassName("ok");
+	} else {
+		ps.up().removeClassName("fieldWithErrors");
+		ps.up().removeClassName("ok");
+		ps.up().addClassName("nok");
+	}
+
+	var psc = $('user_password_confirmation');
+	if(!psc.up().hasClassName('fieldWithErrors') && check(psc.value, "password") && psc.value == $('user_password').value) {
+		psc.up().removeClassName("nok");
+		psc.up().addClassName("ok");
+	} else {
+		psc.up().removeClassName("fieldWithErrors");
+		psc.up().removeClassName("ok");
+		psc.up().addClassName("nok");
+	}
+
+	var sb = $('form_submit');
+	if ($$('.ok').length == 6) {
+		sb.enable();
+	} else {
+		sb.disable();
 	}
 }
 
 
 Event.onDOMReady(function() {
-	$$('p.check').each(function(p) {
-		p.addClassName("nok");
-	});
+
+
+	var error_fields = $$('div.fieldWithErrors');
+	if (error_fields.length > 0) error_fields[0].down().focus();
+
+	check_form();
+
 	
-	$('user_firstname').observe('keyup', function(event) {
-		if(check(this.value, "name")){
-			this.up().removeClassName("nok");
-			this.up().addClassName("ok");
-		} else {
-			this.up().removeClassName("ok");
-			this.up().addClassName("nok");
-		}
-		
-		check_form();
-	});
+	$('user_firstname').observe('keyup', check_form );
 
-
-	$('user_lastname').observe('keyup', function(event) {
-		if(check(this.value, "name")){
-			this.up().removeClassName("nok");
-			this.up().addClassName("ok");
-		} else {
-			this.up().removeClassName("ok");
-			this.up().addClassName("nok");
-		}
-		
-		check_form();
-	});
+	$('user_lastname').observe('keyup', check_form);
 	
-	$('user_email').observe('keyup', function(event) {
-		if(check(this.value, "email")){
-			this.up().removeClassName("nok");
-			this.up().addClassName("ok");
-		} else {
-			this.up().removeClassName("ok");
-			this.up().addClassName("nok");
-		}
-		
-		check_form();
-	});
+	$('user_email').observe('keyup', check_form);
 
-	$('user_email_confirmation').observe('keyup', function(event) {
-		if(check(this.value, "email") && this.value == $('user_email').value) {
-			this.up().removeClassName("nok");
-			this.up().addClassName("ok");
-		} else {
-			this.up().removeClassName("ok");
-			this.up().addClassName("nok");
-		}
-		
-		check_form();
-	});
+	$('user_email_confirmation').observe('keyup', check_form);
 	
+	$('user_password').observe('keyup', check_form);
 	
-	$('user_password').observe('keyup', function(event) {
-		if(check(this.value, "password")){
-			this.up().removeClassName("nok");
-			this.up().addClassName("ok");
-		} else {
-			this.up().removeClassName("ok");
-			this.up().addClassName("nok");
-		}
-		
-		check_form();
-	});
-
-
-	$('user_password_confirmation').observe('keyup', function(event) {
-		if(check(this.value, "password") && this.value == $('user_password').value) {
-			this.up().removeClassName("nok");
-			this.up().addClassName("ok");
-		} else {
-			this.up().removeClassName("ok");
-			this.up().addClassName("nok");
-		}
-
-		check_form();
-	});
+	$('user_password_confirmation').observe('keyup', check_form);
 
 });
