3f9e60c82a10d832af8e4e168fc5c30ce2812e67
Timothy Kim <timothykim@timothylive.net>
2008-04-17 13:06:59 -0400

    photobook update and front page update

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,3 +1,6 @@
+# require 'zip/zip'
+# require 'zip/zipfilesystem'
+
 class Album < ActiveRecord::Base
   belongs_to      :user, :counter_cache => true
   has_many        :photos
@@ -6,6 +9,7 @@ class Album < ActiveRecord::Base
   validates_presence_of     :name
   validates_presence_of     :user_id
   
+  before_destroy  :purge_photos
   
   def get_key_photo
     if self.key_photo_id
@@ -14,5 +18,50 @@ class Album < ActiveRecord::Base
       photo = self.photos.find(:first)
     end
   end
+  
+  
+  
+  
+=begin
+  def zipfile
+    return "/datafiles/albums/" + self.name.split.joing("_") + self.id + ".zip"
+  end
+
+  def generate_zipfile
+    bundle_filename = "#{RAILS_ROOT}/public/datafiles/" + self.zipfile
+
+    # check to see if the file exists already, and if it does, delete it.
+    if File.file?(bundle_filename)
+      File.delete(bundle_filename)
+    end 
+
+    # set the bundle_filename attribute of this object
+    self.bundle_filename = "/uploads/" + self.zipfile
+
+    # open or create the zip file
+    Zip::ZipFile.open(bundle_filename, Zip::ZipFile::CREATE) {
+      |zipfile|
+      # collect the album's tracks
+      self.photos {
+        |photo|
+          # add each track to the archive, names using the track's attributes
+          zipfile.add( "#{set}/#{track.num}-#{track.filename}", "#{RAILS_ROOT}/public#{track.public_filename}")
+        }
+    }
+
+    # set read permissions on the file
+    File.chmod(0644, bundle_filename)
+
+    # save the object
+    self.save
+
+  end
+
+=end
+
+  private
+    def purge_photos
+      self.photos.each { |photo| photo.destroy }
+    end
 
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -5,8 +5,21 @@ class Photo < ActiveRecord::Base
   has_attachment  :content_type => :image,
                   :storage => :file_system,
                   :max_size => 10.megabytes,
-                  :thumbnails => {:view => '600', :list => "200", :thumb => [75,75]}
+                  :thumbnails => {:view => '600', :list => "200", :thumb => [75,75]},
+                  :path_prefix => 'public/datafiles/albums'
 
   validates_as_attachment
+  
+#broken... horribly borken...  
+  # 
+  # def full_filename(thumbnail = nil)
+  #   file_system_path = self.attachment_options[:path_prefix]
+  #   folder = self.thumbnail.nil? ? 'original' : self.thumbnail
+  #   if folder == 'original'
+  #     File.join(RAILS_ROOT, file_system_path, self.album_id, folder, thumbnail_name_for(thumbnail))
+  #   else
+  #     File.join(RAILS_ROOT, file_system_path, self.album_id, folder, thumbnail_name_for(thumbnail))
+  #   end
+  # end
   
 end
