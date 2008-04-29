67babd4cc583640270ce259837cb3a910b47aa25
Timothy Kim <timothykim@timothylive.net>
2008-04-28 20:51:18 -0400

    previous commit got messed up -_-;;;

---
+++ public/javascripts/photobook/index.js
@@ -0,0 +1,8 @@
+Event.onDOMReady(function() {
+	var IE6 = false /*@cc_on || @_jscript_version < 5.7 @*/;
+	if(IE6) {
+		$$('a.listimage_anchor').each(function (a){
+			a.innerHTML = '<img src="/images/frame-photobook-trans.gif" />';
+		});
+	}
+});
