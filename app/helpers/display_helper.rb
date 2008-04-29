9cfa8f7bafb0ecc06a6534412b27d7e6b4f7e28b
Timothy Kim <timothykim@timothylive.net>
2008-04-28 20:49:50 -0400

    newsletter... static but it's ok -_-;;

--- app/helpers/display_helper.rb
+++ app/helpers/display_helper.rb
@@ -1,9 +1,6 @@
 module DisplayHelper
   include OptionDictionary
 
-  def get_current_theme_css(name="global")
-    return "themes/" + Option.find(:first, :conditions => ["name = ?", option(:theme)]).value + "/#{name}"
-  end
   
   def get_menu
     
