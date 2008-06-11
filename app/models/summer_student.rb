655174ce49b09559803bee62af4344a02aec99c8
Timothy Kim <highwind@new-host-2.home>
2008-06-11 03:55:14 -0400

    sorting issue fixed

--- app/models/summer_student.rb
+++ app/models/summer_student.rb
@@ -1,4 +1,8 @@
 class SummerStudent < ActiveRecord::Base
   belongs_to      :summer_school
   has_many        :summer_student_schedule
+
+  def <=>(right)
+    return self.name <=> right.name
+  end
 end
