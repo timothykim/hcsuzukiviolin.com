3bf1f9ddaeafd79368375697303bd00cbb7d1acd
Timothy Kim <highwind@terra.local>
2009-05-25 20:57:48 -0400

    past due date registration is now possible

--- app/controllers/register_controller.rb
+++ app/controllers/register_controller.rb
@@ -26,7 +26,7 @@ class RegisterController < ApplicationController
     @section_title = name
     @submenu = global_submenu
     
-    if current_user.is_admin or (Date.today <= @current_session.due_date and @current_session.is_active)
+#    if current_user.is_admin or (Date.today <= @current_session.due_date and @current_session.is_active)
       
       @student = Student.new
       @student = Student.find(params[:student]) if params[:student]
@@ -77,9 +77,9 @@ class RegisterController < ApplicationController
       
       @javascript_code = "var registration_id = " + (@registration.id || 0).to_s
 
-    else
-      @error = "<img src=\"/images/icons/exclamation.png\" class=\"icon\"/> Sorry, #{name} session is no longer accepting registration."
-    end
+#    else
+#      @error = "<img src=\"/images/icons/exclamation.png\" class=\"icon\"/> Sorry, #{name} session is no longer accepting registration."
+#    end
   end
   
   
