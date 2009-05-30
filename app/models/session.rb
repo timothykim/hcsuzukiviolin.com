8b168faf2125231b9db5c0ccfb951588ad40d06c
Timothy Kim <highwind@terra.local>
2009-05-30 03:38:24 -0400

    registration ordering change

--- app/models/session.rb
+++ app/models/session.rb
@@ -2,9 +2,9 @@ class Session < ActiveRecord::Base
   has_many :weekly_availablities, :dependent => :destroy
   has_many :session_days, :dependent => :destroy
   has_many :registration_options, :dependent => :destroy
-  has_many :registrations, :order => "updated_at DESC"
+  has_many :registrations, :order => "created_at DESC"
   has_many :students, :through => :registrations
-  has_many :lessons, :through => :registrations
+  has_many :lessons, :through => :registrations, :order => "time ASC"
   
   DAY_TYPE = 0
   DATE_TYPE = 1
