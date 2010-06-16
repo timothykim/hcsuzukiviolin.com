705d7ad24d9165ee12c1bb99ffed9b3ef86b3bc8
Timothy Kim <timothykim@timothylive.net>
2010-06-15 23:46:38 -0400

    got rid of authenticity token filter

--- app/controllers/admin/lesson_controller.rb
+++ app/controllers/admin/lesson_controller.rb
@@ -1,4 +1,6 @@
 class Admin::LessonController < AdminController
+  skip_before_filter :verify_authenticity_token
+
   def index
     @section_path = "Adminitration &raquo; "
     @section_title = 'Lessons'
